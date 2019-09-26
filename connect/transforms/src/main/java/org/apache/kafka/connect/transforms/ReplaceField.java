5b50d3ac7763738abc1742b2eb7bc4545a806bdf
Alberto Fedrici <alberto.fedrici@chili.com>
2019-09-26 15:38:28 +0200

    DAG-661 Add check for null values on ReplaceField transformer

--- connect/transforms/src/main/java/org/apache/kafka/connect/transforms/ReplaceField.java
+++ connect/transforms/src/main/java/org/apache/kafka/connect/transforms/ReplaceField.java
@@ -123,6 +123,9 @@ public abstract class ReplaceField<R extends ConnectRecord<R>> implements Transf
 
     @Override
     public R apply(R record) {
+        if (record.value() == null)
+            return record;
+
         if (operatingSchema(record) == null) {
             return applySchemaless(record);
         } else {
